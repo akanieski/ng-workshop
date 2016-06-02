ea3a4d5a0a9a03e97e036f968f2ac927a3d1531e
Andrew Kanieski <andrew.legacy@gmail.com>
2016-06-02 11:41:20 -0400

    auth

--- sandbox2/src/app.ts
+++ sandbox2/src/app.ts
@@ -1,6 +1,7 @@
 import {EmployeeListCtrl} from "./components/employee-list/employee-list-controller"
 import {EmployeeDetailCtrl} from "./components/employee-detail/employee-detail-controller"
 import {EmployeeService} from "./services/employee-service"
+import {NavbarComponent} from "./components/navbar/navbar-component"
 
 export class MainApp {
     
@@ -9,7 +10,7 @@ export class MainApp {
         /* Begin application setup here */
         
         angular
-            .module("SampleApp", ['ngRoute'])
+            .module("SampleApp", ['ngRoute', 'AdalAngular'])
             
             .constant("config", <IConfig>{
                 baseUrl: "http://127.0.0.1:5002"
@@ -17,23 +18,55 @@ export class MainApp {
             
             .service("EmployeeService", EmployeeService)
             
+            .component("navBar", new NavbarComponent())
+            
+            .config(['$locationProvider', function($locationProvider) {
+                    $locationProvider.html5Mode(true).hashPrefix('$');
+                }])
+            
+            .config([
+                '$httpProvider', 
+                'adalAuthenticationServiceProvider', 
+                ($httpProvider: ng.IHttpProvider, adalProvider: adal.AdalAuthenticationServiceProvider) => {
+
+                    adalProvider.init({
+                        instance: 'https://login.microsoftonline.com/', 
+                        tenant: 'nypworkshop.onmicrosoft.com',
+                        clientId: '4efcbe41-4ab8-4b31-b036-e3af3f837d29',
+                        extraQueryParameter: 'nux=1',
+                        //cacheLocation: 'localStorage',
+                        requireADLogin: false,
+                        anonymousEndpoints: ["/"]
+                    }, $httpProvider);
+                
+                }])
+            
             .config(['$routeProvider', function($routeProvider: ng.route.IRouteProvider){
                 
                 $routeProvider
-                
+                    
+                    .when("/landing", <ng.route.IRoute>{
+                        controller: EmployeeDetailCtrl,
+                        controllerAs: "vm",
+                        templateUrl: "src/components/landing/landing.html",
+                        requireADLogin: false
+                    })
+                    
                     .when("/employees", <ng.route.IRoute>{
                         controller: EmployeeListCtrl,
                         controllerAs: "vm",
-                        templateUrl: "src/components/employee-list/employee-list.html"
+                        templateUrl: "src/components/employee-list/employee-list.html",
+                        requireADLogin: true
                     })
                     
                     .when("/employee/:employeeId", <ng.route.IRoute>{
                         controller: EmployeeDetailCtrl,
                         controllerAs: "vm",
-                        templateUrl: "src/components/employee-detail/employee-detail.html"
+                        templateUrl: "src/components/employee-detail/employee-detail.html",
+                        requireADLogin: true
                     })
                     
-                    .otherwise("/employees")
+                    .otherwise("/landing")
             }])
             
         angular.bootstrap("body", ["SampleApp"]);
